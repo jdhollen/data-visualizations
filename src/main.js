e98c20951a6361f76f077064a2dcdbd8b2576e01
jdhollen <james.d.hollenbach@gmail.com>
2018-05-23 11:51:32 -0700

    Add a notion of a 'clicked' county--currently not highlighted.
    This required switching to all numbers for county indexing.

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ const canvas = document.getElementById('map');
 const canvasPath = d3.geoPath().context(context);
 let countyNames = {};
 let alertNames = {};
-let selectedCounty = '';
+let selectedCounty = 0;
+let clickedCounty = 0;
 let previous = {};
 let paused = false;
 let pausedBeforeInputStarted = false;
@@ -29,7 +30,7 @@ let rewind = false;
 const min = 1514764800000;
 const max = 1525132800000;
 const positionSteps = 1000;
-const countyFeatures = {};
+const countyFeatures = [];
 
 const rewindButton = document.getElementById('rewind');
 const backButton = document.getElementById('oneBackward');
@@ -181,9 +182,9 @@ function refreshHoverText() {
   }
 
   // TODO(jdhollen): flush out all missing names + fix.
-  const stateName = countyNames[`${selectedCounty.substring(0, 2)}000`];
+  const stateName = countyNames[selectedCounty - (selectedCounty % 1000)];
   const fullName = `${countyNames[selectedCounty]}, ${stateName}`;
-  const classes = previous[Number(selectedCounty)] || [];
+  const classes = previous[selectedCounty] || [];
 
   let alerts = '';
   for (let i = 0; i < classes.length; i += 1) {
@@ -250,14 +251,20 @@ function redraw(ignorePreviousState) {
   const changeKeys = Object.keys(changes);
   for (let i = 0; i < changeKeys.length; i += 1) {
     const countyId = changeKeys[i];
-    const countyString = countyId < 10000 ? `0${countyId}` : countyId;
     const alertForMap = changes[countyId] ? changes[countyId][0] : '';
     const previousAlertForMap = previous[countyId] ? previous[countyId][0] : '';
     if (ignorePreviousState || alertForMap !== previousAlertForMap) {
       const alertString = types[alertForMap & 0xff] + letterBits[alertForMap & 0xff00];
       const color = alertColors[alertString] || '#cccccc';
-      drawCounty(countyFeatures[countyString], color);
+      drawCounty(countyFeatures[countyId], color);
     }
+  }
+
+  if (clickedCounty) {
+    const alertForMap = changes[clickedCounty] ? changes[clickedCounty][0] : '';
+    const alertString = types[alertForMap & 0xff] + letterBits[alertForMap & 0xff00];
+    const color = alertColors[alertString] || '#cccccc';
+    drawCounty(countyFeatures[clickedCounty], color);
   }
 
   previous = newClasses;
@@ -343,6 +350,34 @@ function maybeRunStep() {
 }
 
 function handleMouseOver(e) {
+  if (clickedCounty) {
+    return;
+  }
+
+  const rect = canvas.getBoundingClientRect();
+  const offsetTop = rect.top + document.body.scrollTop;
+  const offsetLeft = rect.left + document.body.scrollLeft;
+
+  const ratio = 1 / (canvas.width / (960 * devicePixelRatio));
+  const x = Math.floor(ratio * (e.clientX - offsetLeft));
+  const y = Math.floor(ratio * (e.clientY - offsetTop));
+
+  if (clicks16) {
+    const id = clicks16[(x * 600) + y];
+    selectedCounty = id;
+  }
+  refreshHoverText();
+}
+
+function handleMouseOut() {
+  if (clickedCounty) {
+    return;
+  }
+  selectedCounty = 0;
+  refreshHoverText();
+}
+
+function handleCanvasClick(e) {
   const rect = canvas.getBoundingClientRect();
   const offsetTop = rect.top + document.body.scrollTop;
   const offsetLeft = rect.left + document.body.scrollLeft;
@@ -354,16 +389,13 @@ function handleMouseOver(e) {
   if (clicks16) {
     const id = clicks16[(x * 600) + y];
     if (id > 0) {
-      selectedCounty = id < 10000 ? `0${id}` : `${id}`;
+      selectedCounty = id;
     } else {
-      selectedCounty = '';
+      selectedCounty = 0;
     }
   }
-  refreshHoverText();
-}
+  clickedCounty = selectedCounty;
 
-function handleMouseOut() {
-  selectedCounty = '';
   refreshHoverText();
 }
 
@@ -501,7 +533,7 @@ function loadMapData(usData) {
   const counties = topojson.feature(us, us.objects.counties).features;
 
   for (let i = 0; i < counties.length; i += 1) {
-    countyFeatures[counties[i].id] = counties[i];
+    countyFeatures[Number(counties[i].id)] = counties[i];
   }
 
   drawBaseMap();
@@ -541,6 +573,7 @@ function main() {
   canvas.addEventListener('mouseover', handleMouseOver);
   canvas.addEventListener('mousemove', handleMouseOver);
   canvas.addEventListener('mouseout', handleMouseOut);
+  canvas.addEventListener('click', handleCanvasClick);
   sizeCanvas();
 
   Promise.all([weather, clicks, alerts, counties, map]).then(() => maybeRunStep());
