a359e025d71467ad9e644811d58ea72f0c9b9e2d
jdhollen <james.d.hollenbach@gmail.com>
2018-05-31 10:17:45 -0700

    Clean up and document legend.

--- src/legend.js
+++ src/legend.js
@@ -1,5 +1,6 @@
 /* eslint-env browser */
 
+/* A simple legend that spits out formatted HTML into the specified DOM element. */
 export default class Legend {
   constructor(legend, countyRenderer, alertRenderer) {
     this.legend = legend;
@@ -7,7 +8,7 @@ export default class Legend {
     this.alertRenderer = alertRenderer;
   }
 
-  setLegendContents(countyId, classes) {
+  setLegendContents(countyId, alertIds) {
     if (!countyId) {
       this.legend.innerHTML = '<span class="legendTitle">Select a county to see alerts.</span>';
       return;
@@ -16,12 +17,15 @@ export default class Legend {
     const fullName = this.countyRenderer.getFullName(countyId);
 
     let alertHtml = '';
-    for (let i = 0; i < classes.length; i += 1) {
-      const av = classes[i];
+    for (let i = 0; i < alertIds.length; i += 1) {
+      const alertId = alertIds[i];
 
-      const alertColor = this.alertRenderer.getColor(av);
+      const alertColor = this.alertRenderer.getColor(alertId);
+
+      // Some tiny phones (iPhone SE, Moto E, yada yada) don't have the screen
+      // real estate to render "Winter Weather Advisory", so we shorten it up.
       const alert = (window.innerWidth >= 375)
-        ? this.alertRenderer.getFullName(av) : this.alertRenderer.getShortName(av);
+        ? this.alertRenderer.getFullName(alertId) : this.alertRenderer.getShortName(alertId);
       if (alert) {
         alertHtml = alertHtml.concat(`<div class="legendItem"><div class="legendSquare" style="background-color:${alertColor};"></div>${alert}</div>`);
       }
