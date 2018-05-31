bb4959446dd0a37e0b899b3bbb2d9cc67ce2736d
jdhollen <james.d.hollenbach@gmail.com>
2018-05-31 10:09:40 -0700

    Document CountyRenderer

--- src/county-renderer.js
+++ src/county-renderer.js
@@ -1,5 +1,13 @@
 /* eslint class-methods-use-this: ["error", { "exceptMethods": ["getColor"] }] */
 
+/*
+ * A renderer for displaying US Counties as "CountyName, State"--for example,
+ * "Santa Cruz, California" or "Union, New Jersey".  The input map should hold
+ * FIPS->name mappings for both counties and states.  The state names should
+ * be stored in the slot for county code 000.  For example, "1000" should be
+ * Alabama.  FIPS codes are NOT zero-padded in this map (they look like actual
+ * numbers).  That means Alabama is "1000", not "01000".
+ */
 export default class CountyRenderer {
   constructor(countyNames) {
     this.countyNames = countyNames;
